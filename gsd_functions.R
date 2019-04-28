2e27070122c399e02db47f98a90ed1ce1d3218dd
Dong Xi <38050865+xidongdxi@users.noreply.github.com>
2019-04-28 11:00:44 -0400

    Update gsd_functions.R

--- gsd_functions.R
+++ gsd_functions.R
@@ -1,11 +1,12 @@
 library(mvtnorm)
 
 # Function to generate the correlation matrix
-# t: information or information time
+## Input
+### t: information time
+## Output
+### Correlation matrix
 cr_function <- function(t){
-  # Number of looks
   K <- length(t)
-  # Correlation matrix based on planned information
   cr <- diag(K)
   for (i in 1:K){
     for (j in i:K){
@@ -14,4 +15,43 @@ cr_function <- function(t){
   }
   cr <- cr + t(cr) - diag(K)
   return(cr)
+}
+
+# Function to derive the stopping boundary using the cumulative error spent
+## Input
+### alpha: one-sided significance level
+### t: information time
+### cumulative: cumulative error spent with the same length as t
+## Output
+### Vector of boundary values
+solver_boundary_esf_function <- function(alpha, t, cumulative){
+  K <- length(t)
+  cr <- cr_function(t)
+  solver <- function(x, cumu, cr = cr, c_past){
+    z <- c(c_past, x)
+    return(1 - cumu - pmvnorm(upper = z, corr = cr[1:length(z), 1:length(z)],
+                              algorithm = Miwa(steps = 128))
+    )
+  }
+  c_boundary <- rep(0,K)
+  c_boundary[1] <- min(qnorm(1 - cumulative[1]), 10)
+  if (K>1){
+    for (k in 2:K){
+      a <- uniroot(solver, interval=c(0.001, 10), cumu = cumulative[k],
+                   cr = cr, c_past = c_boundary[1:(k-1)])$root
+      c_boundary[k] <- min(a, 10)
+    }
+  }
+  return(c_boundary)
+}
+
+# Function to calculate the conditional error rate
+## Input
+### t: information time
+### c_boundary: boundary values
+## Output
+### Vecotr of the conditional error rate
+CE_function <- function(t, c_boundary) {
+  K <- length(t)
+  1 - pnorm((c_boundary[K] - c_boundary * sqrt(t)) / sqrt(1 - t))
 }
